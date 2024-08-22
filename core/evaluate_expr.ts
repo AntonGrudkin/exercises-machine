d0c763311527ae42b9b8b6a88d10c5a29aa8de76
dvodopian <dgvodopyan@gmail.com>
2024-08-22 17:31:07 +0300

    removed unused files

--- core/evaluate_expr.ts
+++
@@ -1,6 +0,0 @@
-
-export function evalExprStr(input: string): number {
-    if(input === '1=1')
-        return 1
-    else throw SyntaxError(`${input}`)
-}
--- test/main.test.ts
+++
@@ -1,9 +0,0 @@
-import assert from 'assert';
-import {evalExprStr} from '../core/evaluate_expr'
-
-describe('eval_expr_str', function() {
-    it('works for 1=1', function() {
-        const result = evalExprStr("1=1")
-        assert.equal(result, 1);
-    });
-});
